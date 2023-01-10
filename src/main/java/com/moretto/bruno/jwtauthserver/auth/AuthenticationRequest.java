5670e53ce857f08c24ddc647ec802e9b6b2e35fe
Bruno Moretto <bmoretto404@gmail.com>
2023-01-09 22:17:47 -0300

    Project development

---
+++ src/main/java/com/moretto/bruno/jwtauthserver/auth/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.moretto.bruno.jwtauthserver.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationRequest {
+
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/moretto/bruno/jwtauthserver/user/Role.java
@@ -0,0 +1,8 @@
+package com.moretto.bruno.jwtauthserver.user;
+
+public enum Role {
+
+    USER,
+    ADMIN;
+
+}
